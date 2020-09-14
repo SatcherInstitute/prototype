c7a41882237ba223a81164e8019dc4ba5a83a73a
Brian Suk <briansuk@google.com>
2020-09-14 00:32:29 -0400

    Initial build of SQL query joining CDC provisional death rates, county boundary data, and urgent care facility data.

---
+++ sql/pdccr.sql
@@ -0,0 +1,57 @@
+# Create query scope persistent PDCCR table with constant UUID.
+# Table is joined with BQ PDP Census Counties table, joined by FIPS Code.
+CREATE temp table cdc_pdccr_uuid_base AS(
+  SELECT
+    generate_uuid() AS uuid,
+    geo.county_geom AS geo_county_geom,
+    geo.lsad_name AS geo_lsad_name,
+    LPAD(CAST(fips_code AS STRING), 5, "0") AS fips,
+    cdc_pdccr.*
+  FROM
+    `fellowship-test-internal.suk_sandbox.cdc_provisional_death_count_county_race` cdc_pdccr
+  JOIN
+    `bigquery-public-data.geo_us_boundaries.counties` geo
+  ON
+    LPAD(CAST(fips_code AS STRING), 5, "0") = geo.county_fips_code);
+
+# Create intermediary table that joins with the Urgent Care Facility data,
+# joined by FIPS Code, and creates REPEATED column with urgent care facilities
+# that match the FIPS Code.
+WITH
+cdc_pdccr_urgent_care AS (
+  SELECT
+    cdc_pdccr_uuid_base.uuid,
+    ARRAY_AGG(
+      STRUCT( ucf.id,
+        ucf.name,
+        st_geogpoint(ucf.x_26, ucf.y_27) as geog_pt,
+        ucf.address,
+        ucf.address2,
+        ucf.city,
+        ucf.state,
+        ucf.zip,
+        ucf.zipp4,
+        ucf.county,
+        ucf.fips,
+        ucf.directions) 
+      IGNORE NULLS ) AS ufc_ids
+  FROM
+    cdc_pdccr_uuid_base
+  LEFT OUTER JOIN
+    `fellowship-test-internal.suk_sandbox.urgent_care_facilities` ucf
+  ON
+    cdc_pdccr_uuid_base.fips = CAST(ucf.fips AS string)
+  GROUP BY
+    cdc_pdccr_uuid_base.uuid )
+
+# Join back with original PDCCR table to get all the remaining columns,
+# and count the number of UCF locations for each row.
+SELECT
+  cdc_pdccr_uuid_base.*,
+  if(cdc_pdccr_urgent_care.ufc_ids[OFFSET(0)].name is null, 0, ARRAY_LENGTH(cdc_pdccr_urgent_care.ufc_ids)) as uCF_count,
+  cdc_pdccr_urgent_care.ufc_ids
+FROM
+  cdc_pdccr_uuid_base,
+  cdc_pdccr_urgent_care
+WHERE
+  cdc_pdccr_uuid_base.uuid = cdc_pdccr_urgent_care.uuid
