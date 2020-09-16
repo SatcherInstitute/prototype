067b65f07035c0b7c61017c182d6fe01f4d9f4d7
Brian Suk <briansuk@google.com>
2020-09-15 21:10:45 -0400

    Updated from comments made on PR#22

--- prototype.tf
+++ prototype.tf
@@ -13,6 +13,7 @@ resource "google_storage_bucket" "gcs_data_ingestion_landing_bucket" {
   name                    = var.gcs_landing_bucket
   location                = var.gcs_region
   force_destroy           = true # This forces deletion of objects created in bucket post provisioning
+    # https://www.terraform.io/docs/providers/google/r/storage_bucket.html#force_destroy
 }
 
 /*
