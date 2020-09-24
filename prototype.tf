2508acf6da08306da67878e348f76a5ee82e591e
mayaspivak <mspivak@google.com>
2020-09-24 13:50:18 -0700

    Finish ingestion service account configuration in terraform (#34)
    Added an ingestion runner service account and gave it a custom role with permissions to access GCS and pubsub while running the cloud run service.

--- prototype.tf
+++ prototype.tf
@@ -1,7 +1,7 @@
 # Specify the provider, here
 provider "google" {
-  project                 = var.project_id
-  region                  = var.compute_region
+  project = var.project_id
+  region  = var.compute_region
 }
 
 /* 
@@ -10,16 +10,16 @@ provider "google" {
 
 # Raw data landing zone for data ingestion
 resource "google_storage_bucket" "gcs_data_ingestion_landing_bucket" {
-  name                    = var.gcs_landing_bucket
-  location                = var.gcs_region
-  force_destroy           = true # This forces deletion of objects created in bucket post provisioning
-    # https://www.terraform.io/docs/providers/google/r/storage_bucket.html#force_destroy
+  name          = var.gcs_landing_bucket
+  location      = var.gcs_region
+  force_destroy = true # This forces deletion of objects created in bucket post provisioning
+  # https://www.terraform.io/docs/providers/google/r/storage_bucket.html#force_destroy
 }
 
 # Bucket for storing the GCF code
 resource "google_storage_bucket" "gcf_code" {
-  name                    = var.gcf_code_bucket
-  location                = var.gcs_region
+  name     = var.gcf_code_bucket
+  location = var.gcs_region
 }
 
 /*
@@ -34,29 +34,29 @@ resource "google_storage_bucket" "gcf_code" {
  */
 
 resource "google_storage_bucket_object" "file_pdccr" {
- name                     = "covid_19_pdccr.csv"
- bucket                   = google_storage_bucket.gcs_data_ingestion_landing_bucket.name
- source                   = "./testing_data_files/Provisional_COVID-19_Death_Counts_by_County_and_Race.csv"
+  name   = "covid_19_pdccr.csv"
+  bucket = google_storage_bucket.gcs_data_ingestion_landing_bucket.name
+  source = "./testing_data_files/Provisional_COVID-19_Death_Counts_by_County_and_Race.csv"
 }
 
 resource "google_storage_bucket_object" "file_ucf" {
- name                     = "Urgent_Care_Facilities.csv"
- bucket                   = google_storage_bucket.gcs_data_ingestion_landing_bucket.name
- source                   = "./testing_data_files/Urgent_Care_Facilities.csv"
+  name   = "Urgent_Care_Facilities.csv"
+  bucket = google_storage_bucket.gcs_data_ingestion_landing_bucket.name
+  source = "./testing_data_files/Urgent_Care_Facilities.csv"
 }
 
 /*
  * [END] Test File Upload
  */
 
- /*
-  * [BEGIN] BigQuery Setup
-  */
+/*
+ * [BEGIN] BigQuery Setup
+ */
 
 # Create a BigQuery dataset
 resource "google_bigquery_dataset" "bq_dataset" {
-  dataset_id              = var.bq_dataset_name
-  location                = "US"
+  dataset_id = var.bq_dataset_name
+  location   = "US"
 }
 
 /*
@@ -73,7 +73,7 @@ resource "google_bigquery_table" "bq_table_pdccr" {
 
 # PDCCR load
 resource "google_bigquery_job" "bq_job_load_pdccr" {
-  job_id     = "pdccr_load${formatdate("YYYYMMDDhhmmss",timestamp())}"
+  job_id = "pdccr_load${formatdate("YYYYMMDDhhmmss", timestamp())}"
 
   load {
     source_uris = [
@@ -89,7 +89,7 @@ resource "google_bigquery_job" "bq_job_load_pdccr" {
     skip_leading_rows = 1
 
     write_disposition = "WRITE_TRUNCATE"
-    autodetect = true
+    autodetect        = true
   }
 }
 
@@ -101,7 +101,7 @@ resource "google_bigquery_table" "bq_table_ucf" {
 
 # UCF load
 resource "google_bigquery_job" "bq_job_load_ucf" {
-  job_id     = "ucf_load${formatdate("YYYYMMDDhhmmss",timestamp())}"
+  job_id = "ucf_load${formatdate("YYYYMMDDhhmmss", timestamp())}"
 
   load {
     source_uris = [
@@ -117,7 +117,7 @@ resource "google_bigquery_job" "bq_job_load_ucf" {
     skip_leading_rows = 1
 
     write_disposition = "WRITE_TRUNCATE"
-    autodetect = true
+    autodetect        = true
   }
 }
 
@@ -136,7 +136,7 @@ resource "google_bigquery_table" "bqt_pdccr_ucf_joined" {
 # Use the ./sql/pdccr.sql file
 resource "google_bigquery_job" "bq_job_pdccr_ucf_joined" {
   depends_on = [google_bigquery_job.bq_job_load_pdccr, google_bigquery_job.bq_job_load_ucf]
-  job_id     = "pdccrucfjoin${formatdate("YYYYMMDDhhmmss",timestamp())}"
+  job_id     = "pdccrucfjoin${formatdate("YYYYMMDDhhmmss", timestamp())}"
 
   query {
     query = file("./sql/pdccr.sql")
@@ -159,31 +159,31 @@ resource "google_bigquery_job" "bq_job_pdccr_ucf_joined" {
 
 # Create a ZIP of the data_ingestion folder.
 data "archive_file" "upload_to_gcs_zip" {
- type                     = "zip"
- source_dir               = "${var.gcf_code_path}/data_ingestion/"
- output_path              = "${var.gcf_code_path}/upload_to_gcs.zip"
+  type        = "zip"
+  source_dir  = "${var.gcf_code_path}/data_ingestion/"
+  output_path = "${var.gcf_code_path}/upload_to_gcs.zip"
 }
 
 # Place the ZIP file into the gcf_code bucket
 resource "google_storage_bucket_object" "upload_to_gcs_code" {
- name                     = "upload_to_gcs.zip"
- bucket                   = google_storage_bucket.gcf_code.name
- source                   = data.archive_file.upload_to_gcs_zip.output_path 
+  name   = "upload_to_gcs.zip"
+  bucket = google_storage_bucket.gcf_code.name
+  source = data.archive_file.upload_to_gcs_zip.output_path
 }
 
 # Configure the actual Cloud Function for uploading data to GCS
 resource "google_cloudfunctions_function" "data_ingestion_to_gcs" {
-  name                     = var.gcf_upload_to_gcs_name
-  description              = "Downloads data files from the internet and uploads to a GCS bucket"
-  available_memory_mb      = 256
-  source_archive_bucket    = google_storage_bucket.gcf_code.name
-  source_archive_object    = google_storage_bucket_object.upload_to_gcs_code.name
-  timeout                  = 120
-  entry_point              = "ingest_data"
-  runtime                  = "python37"
+  name                  = var.gcf_upload_to_gcs_name
+  description           = "Downloads data files from the internet and uploads to a GCS bucket"
+  available_memory_mb   = 256
+  source_archive_bucket = google_storage_bucket.gcf_code.name
+  source_archive_object = google_storage_bucket_object.upload_to_gcs_code.name
+  timeout               = 120
+  entry_point           = "ingest_data"
+  runtime               = "python37"
   event_trigger {
-     event_type            = "google.pubsub.topic.publish"
-     resource              = google_pubsub_topic.upload_to_gcs.name
+    event_type = "google.pubsub.topic.publish"
+    resource   = google_pubsub_topic.upload_to_gcs.name
   }
 }
 
@@ -191,50 +191,84 @@ resource "google_cloudfunctions_function" "data_ingestion_to_gcs" {
  * [END] GCF Setup
  */
 
- /*
-  * [BEGIN] Cloud Run Setup
-  */
+/*
+ * [BEGIN] Service Account Setup
+ */
 
 # Service account used to invoke the cloud run service through the push subscription.
-resource "google_service_account" "ingestion_identity" {
-  account_id = var.ingestion_identity_id
+resource "google_service_account" "ingestion_invoker_identity" {
+  # The account id that is used to generate the service account email. Must be 6-30 characters long and
+  # match the regex [a-z]([-a-z0-9]*[a-z0-9]).
+  account_id = var.ingestion_invoker_identity_id
 }
+
+# Service account whose identity is used when running the ingestion service.
+resource "google_service_account" "ingestion_runner_identity" {
+  # The account id that is used to generate the service account email. Must be 6-30 characters long and
+  # match the regex [a-z]([-a-z0-9]*[a-z0-9]).
+  account_id = var.ingestion_runner_identity_id
+}
+
+# Give the ingestion invoker service account the existing invoker role so that it can call the ingestion service.
+resource "google_cloud_run_service_iam_member" "ingestion_invoker_binding" {
+  location = var.compute_region
+  service  = google_cloud_run_service.ingestion_service.name
+  role     = "roles/run.invoker"
+  member   = format("serviceAccount:%s", google_service_account.ingestion_invoker_identity.email)
+}
+
+# Give the ingestion runner service account permissions it needs (e.g. GCS bucket access). Add to the permissions list
+# here if the ingestion runner needs access to other GCP resources.
+resource "google_project_iam_custom_role" "ingestion_runner_role" {
+  role_id     = var.ingestion_runner_role_id
+  title       = "Ingestion Runner"
+  description = "Allows data upload to GCS bucket and pubsub publish to notify completion"
+  permissions = ["storage.objects.create", "storage.objects.delete", "storage.objects.get", "storage.objects.list",
+  "storage.objects.update", "storage.buckets.get", "pubsub.topics.publish"]
+}
+
+resource "google_project_iam_member" "ingestion_runner_binding" {
+  project = var.project_id
+  role    = google_project_iam_custom_role.ingestion_runner_role.id
+  member  = format("serviceAccount:%s", google_service_account.ingestion_runner_identity.email)
+}
+
+/* 
+ * [END] Service Account Setup 
+ */
+
+/*
+ * [BEGIN] Cloud Run Setup
+ */
 
 # Push subscription for upload_to_gcs topic that invokes the run service.
 resource "google_pubsub_subscription" "ingestion_subscription" {
-  name = var.ingestion_subscription_name
+  name  = var.ingestion_subscription_name
   topic = google_pubsub_topic.upload_to_gcs.name
-  
+
   ack_deadline_seconds = 20
-  
+
   push_config {
+    # Due to Terraform config language restrictions, index the first status element in a list of one.
     push_endpoint = google_cloud_run_service.ingestion_service.status.0.url
     oidc_token {
-      service_account_email = google_service_account.ingestion_identity.email
+      service_account_email = google_service_account.ingestion_invoker_identity.email
     }
   }
-}
-
-# Give the ingestion service account the invoker role so that it can call the ingestion service.
-resource "google_cloud_run_service_iam_binding" "ingestion_service_binding" {
-  location = var.compute_region
-  service = google_cloud_run_service.ingestion_service.name
-  role = "roles/run.invoker"
-  members = [
-    format("serviceAccount:%s", google_service_account.ingestion_identity.email),
-  ]
 }
 
 # Cloud Run service for uploading data to gcs.
 resource "google_cloud_run_service" "ingestion_service" {
   name     = var.run_ingestion_service_name
   location = var.compute_region
+  project  = var.project_id
 
   template {
     spec {
       containers {
         image = var.run_ingestion_image_path
       }
+      service_account_name = google_service_account.ingestion_runner_identity.email
     }
   }
 
@@ -247,7 +281,7 @@ resource "google_cloud_run_service" "ingestion_service" {
 
 /* 
  * [END] Cloud Run Setup
- */  
+ */
 
 /*
  * [BEGIN] Cloud Scheduler Setup
@@ -255,24 +289,24 @@ resource "google_cloud_run_service" "ingestion_service" {
 
 # Create a Pub/Sub topic to trigger data_ingestion_to_gcs.
 resource "google_pubsub_topic" "upload_to_gcs" {
-  name                    = var.upload_to_gcs_topic_name
+  name = var.upload_to_gcs_topic_name
 }
 
 # Create a Cloud Scheduler task to trigger the upload_to_gcs Pub/Sub event for household income data.
 resource "google_cloud_scheduler_job" "household_income_scheduler" {
-  name                    = var.household_income_scheduler_name
-  description             = "Triggers uploading household income data from SAIPE to GCS every Thursday at 8:10 ET."
-  time_zone               = "America/New_York"
-  schedule                = "10 8 * * 5"
+  name        = var.household_income_scheduler_name
+  description = "Triggers uploading household income data from SAIPE to GCS every Thursday at 8:10 ET."
+  time_zone   = "America/New_York"
+  schedule    = "10 8 * * 5"
 
   pubsub_target {
-    topic_name            = google_pubsub_topic.upload_to_gcs.id
-    data                  = base64encode(jsonencode({
-                              "id":"HOUSEHOLD_INCOME",
-                              "url":"https://api.census.gov/data/timeseries/poverty/saipe",
-                              "gcs_bucket": google_storage_bucket.gcs_data_ingestion_landing_bucket.name,
-                              "filename":"SAIPE"
-                            }))
+    topic_name = google_pubsub_topic.upload_to_gcs.id
+    data = base64encode(jsonencode({
+      "id" : "HOUSEHOLD_INCOME",
+      "url" : "https://api.census.gov/data/timeseries/poverty/saipe",
+      "gcs_bucket" : google_storage_bucket.gcs_data_ingestion_landing_bucket.name,
+      "filename" : "SAIPE"
+    }))
   }
 }
 
--- prototype_variables.tf
+++ prototype_variables.tf
@@ -1,53 +1,53 @@
 variable "project_id" {
-  description   = "Google Project ID"
-  type          = string
+  description = "Google Project ID"
+  type        = string
 }
 
 variable "compute_region" {
-  description   = "Region for Compute Resources"
-  type          = string
-  default       = "us-central1"
+  description = "Region for Compute Resources"
+  type        = string
+  default     = "us-central1"
 }
 
 variable "gcs_landing_bucket" {
-  description   = "Name of the landing GCS bucket"
-  type          = string
+  description = "Name of the landing GCS bucket"
+  type        = string
 }
 
 variable "gcs_region" {
-  description   = "Region for Google Cloud Storage"
-  type          = string
-  default       = "US"
+  description = "Region for Google Cloud Storage"
+  type        = string
+  default     = "US"
 }
 
 variable "bq_dataset_name" {
-  description   = "BigQuery Main Dataset"
-  type          = string
+  description = "BigQuery Main Dataset"
+  type        = string
 }
 
 variable "gcf_code_path" {
-  description   = "Base path where the Cloud Functions code lives"
-  type          = string
+  description = "Base path where the Cloud Functions code lives"
+  type        = string
 }
 
 variable "gcf_code_bucket" {
-  description   = "Name of the bucket where the GCF code is kept"
-  type          = string
+  description = "Name of the bucket where the GCF code is kept"
+  type        = string
 }
 
 variable "upload_to_gcs_topic_name" {
-  description   = "Name of the Pub/Sub topic used to trigger uploading files to GCS"
-  type          = string
+  description = "Name of the Pub/Sub topic used to trigger uploading files to GCS"
+  type        = string
 }
 
 variable "gcf_upload_to_gcs_name" {
-  description   = "Name of the GCF function which uploads files to GCS"
-  type          = string
+  description = "Name of the GCF function which uploads files to GCS"
+  type        = string
 }
 
 variable "household_income_scheduler_name" {
-  description   = "Name of the Cloud Scheduler job for downloading household income data"
-  type          = string
+  description = "Name of the Cloud Scheduler job for downloading household income data"
+  type        = string
 }
 
 variable "run_ingestion_service_name" {
@@ -60,12 +60,27 @@ variable "run_ingestion_image_path" {
   type        = string
 }
 
-variable "ingestion_identity_id" {
+variable "ingestion_invoker_identity_id" {
   description = "Account id of the service account used to trigger data ingestion"
-  type = string
+  type        = string
 }
 
 variable "ingestion_subscription_name" {
   description = "Name of push subscription that invokes the ingestion service"
-  type = string
+  type        = string
+}
+
+variable "ingestion_runner_identity_id" {
+  description = "Account id of the service account used when running the data ingestion service"
+  type        = string
+}
+
+variable "project_number" {
+  description = "Google project number"
+  type        = number
+}
+
+variable "ingestion_runner_role_id" {
+  description = "Id of custom role given to the ingestion runner service account"
+  type        = string
 }
