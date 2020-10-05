f92394412e3a02aeb1bbb6c06aab3f7089b5c3cf
kkatzen <kristak@google.com>
2020-10-05 11:59:48 -0700

    Updating multiple URL upload function

--- data_ingestion/common/di_url_file_to_gcs.py
+++ data_ingestion/common/di_url_file_to_gcs.py
@@ -20,6 +20,16 @@ def url_file_to_gcs(url, url_params, gcs_bucket, dest_filename):
     dest_filename: What to name the downloaded file in GCS. Include the file extension."""
   download_first_url_to_gcs([url], url_params, gcs_bucket, dest_filename)
 
+def get_first_response(url_list, url_params):
+  for url in url_list:
+    try:
+      file_from_url = requests.get(url, params=url_params)
+      file_from_url.raise_for_status()
+      return file_from_url
+    except requests.HTTPError as err:
+      logging.error("HTTP error for url %s: %s", url, err)
+  return None
+ 
 def download_first_url_to_gcs(url_list, url_params, gcs_bucket, dest_filename):
   """Iterates over the list of potential URLs that may point to the data source until one of the URLs succeeds in downloading.
     If no URL suceeds, the method will return an error.
@@ -37,16 +47,10 @@ def download_first_url_to_gcs(url_list, url_params, gcs_bucket, dest_filename):
     logging.error("GCS Bucket %s not found", gcs_bucket)
 
   # Find a valid file in the URL list or exit
-  for url in url_list:
-    try:
-      file_from_url = requests.get(url, params=url_params)
-      file_from_url.raise_for_status()
-      break;
-    except requests.HTTPError as err:
-      logging.error("HTTP error for url %s: %s", url, err)
-  if(file_from_url is None):
-    logging.error("No file could be found")
-    return;
+  file_from_url = get_first_response(url_list, url_params)
+  if file_from_url is None:
+      logging.error("No file could be found for intended destination: %s", dest_filename)
+      return;
   
   # Download URL locally, upload to bucket and remove local file
   local_path = local_file_path(dest_filename)
