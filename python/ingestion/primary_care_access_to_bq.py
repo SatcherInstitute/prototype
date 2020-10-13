0c1567c17f9b2e46c9f426b75a66f5adf15afef2
kkatzen <kristak@google.com>
2020-10-13 11:39:25 -0700

    Consolidate primary care access BQ jobs

--- python/ingestion/primary_care_access_to_bq.py
+++ python/ingestion/primary_care_access_to_bq.py
@@ -21,6 +21,7 @@ def write_primary_care_access_to_bq(dataset, table_name, gcs_bucket, fileprefix)
   client = storage.Client()
   bucket = client.get_bucket(gcs_bucket)
 
+  data = []
   for state_name in _STATE_NAMES:
     filename = _FILEPATH.format(fileprefix, state_name)
     blob = bucket.blob(filename)
@@ -28,25 +29,24 @@ def write_primary_care_access_to_bq(dataset, table_name, gcs_bucket, fileprefix)
     blob.download_to_filename(local_path)
 
     frame = read_excel(io=local_path, sheet_name='Ranked Measure Data', skiprows=[0,2])
-    data = []
     for row_index, row in frame.iterrows():
       # These fields may not be set for every county, if they're not set, we'll use -1 as the numerical value
       num_primary_care_physicians = row[108] if not math.isnan(row[108]) else -1;
       primary_care_physicians_rate = row[109] if not math.isnan(row[108]) else -1;
       row = [row[0], row[1], row[2], num_primary_care_physicians, primary_care_physicians_rate]
       data.append(row)
-    new_dataframe = DataFrame(
-      data=data,
-      columns=('county_fips_code',
-        'state_name',
-        'county_name',
-        'num_primary_care_physicians',
-        'primary_care_physicians_rate'))
-    column_types = {
-      'county_fips_code': 'INT64',
-      'state_name': 'STRING',
-      'county_name': 'STRING',
-      'num_primary_care_physicians': 'FLOAT64', 
-      'primary_care_physicians_rate': 'FLOAT64', 
-    }
-    append_dataframe_to_bq(new_dataframe, dataset, table_name, column_types=column_types)
+  new_dataframe = DataFrame(
+    data=data,
+    columns=('county_fips_code',
+      'state_name',
+      'county_name',
+      'num_primary_care_physicians',
+      'primary_care_physicians_rate'))
+  column_types = {
+    'county_fips_code': 'INT64',
+    'state_name': 'STRING',
+    'county_name': 'STRING',
+    'num_primary_care_physicians': 'FLOAT64', 
+    'primary_care_physicians_rate': 'FLOAT64', 
+  }
+  append_dataframe_to_bq(new_dataframe, dataset, table_name, column_types=column_types)
